afce8c7f3509a465516585ae2f3f1627c0c20271
John Canessa <8019504+JohnCanessa@users.noreply.github.com>
2021-03-29 12:21:29 -0500

    Add files via upload
    Used two approaches and cleaned previous implementation.

--- Solution.java
+++ Solution.java
@@ -1,113 +1,125 @@
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Scanner;
 
+
 /**
- * 
+ * Sum two numbers
  */
-public class Solution {
+class Solution {
 
-  /**
-   * Determine wheater there is a pair of numbers, where one number is taken from
-   * a and the other from b, that can be added together to get a sum of v. Return
-   * true if such a pair exists, otherwise return false.
-   * 
-   * Brure force approach. O(n^2).
-   */
-  static boolean sumOfTwo(int[] a, int[] b, int v) {
 
-    // **** traverse both arrays (O(n^2)) ****
-    for (int i = 0; i < a.length; i++) {
+    /**
+     * Given an array of integers, return indices of the two numbers 
+     * such that they add up to a specific target.
+     * 
+     * You may assume that each input would have exactly one solution, 
+     * and you may not use the same element twice.
+     * 
+     * Runtime: 0 ms
+     * Memory Usage: 38.9 MB
+     */
+    static int[] sumOfTwo1(int[] nums, int target) {
 
-      // **** desired value in array b ****
-      int x = v - a[i];
+        // **** initialization ****
+        int[] result = new int[2];
 
-      // **** look for it in array b ****
-      for (int j = 0; j < b.length; j++) {
-        if (b[j] == x) {
-          return true;
+        // **** generate a hashmap of values and indices O(n) ****
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+
+            // **** generate the complement ****
+            int complement = target - nums[i];
+
+            // **** check if complement in the hasmap ****
+            if (hm.containsKey(complement)) {
+                result[0] = i;
+                result[1] = hm.get(complement);
+                return result;
+            }
+
+            // **** add value to hash map ****
+            hm.putIfAbsent(nums[i], i);
         }
-      }
+
+        // **** instead of returning [0, 0] ****
+        throw new IllegalArgumentException("No two sum solution");
     }
 
-    // **** value not found ****
-    return false;
-  }
 
-  /**
-   * Determine wheater there is a pair of numbers, where one number is taken from
-   * a and the other from b, that can be added together to get a sum of v. Return
-   * true if such a pair exists, otherwise return false.
-   * 
-   * O(n) approach.
-   */
-  static boolean sumOfTwo1(int[] a, int[] b, int v) {
 
-    // **** values of array b ****
-    HashSet<Integer> hs = new HashSet<Integer>();
+    /**
+     * Given an array of integers, return indices of the two numbers 
+     * such that they add up to a specific target.
+     * 
+     * You may assume that each input would have exactly one solution, 
+     * and you may not use the same element twice.
+     * 
+     * Runtime: 0 ms
+     * Memory Usage: 39.3 MB
+     */
+    static int[] sumOfTwo(int[] nums, int target) {
 
-    // **** add the values of array b into the hash set (O(n))****
-    for (int i = 0; i < b.length; i++) {
-      hs.add(b[i]);
+        // **** traverse array O(n) ****
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            // **** traverse array O(n) ****
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        // **** did not find solution ****
+        throw new IllegalArgumentException("No two sum solution");
     }
 
-    // *** traverse array a looking for a value in the hash set (O(n)) ****
-    for (int i = 0; i < a.length; i++) {
 
-      // **** desired value ****
-      int x = v - a[i];
+    /**
+     * Test scafolding.
+     */
+    public static void main(String[] args) {
 
-      // **** look for it in the hash set ****
-      if (hs.contains(x)) {
-        return true;
-      }
+        // **** open scanner ****
+        Scanner sc = new Scanner(System.in);
+
+        // **** read the number of integers in the array ****
+        int n = sc.nextInt();
+
+        // ???? ????
+        System.out.println("main <<< n: " + n);
+
+        // **** allocate array of integers ****
+        int[] arr = new int[n];
+
+        // **** populate the array ****
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        // **** read the target ****
+        int target = sc.nextInt();
+
+        // ???? ????
+        System.out.println("main <<< target: " + target);
+
+        // **** close scanner ****
+        sc.close();
+
+        // ???? ????
+        System.out.println("main <<< arr: " + Arrays.toString(arr));
+
+        // **** generate result (if possible) ****
+        int[] result = sumOfTwo1(arr, target);
+
+        // **** display result ****
+        System.out.println(Arrays.toString(result));
+
+        // **** generate result (if possible) ****
+        result = sumOfTwo(arr, target);
+
+        // **** display result ****
+        System.out.println(Arrays.toString(result));
     }
-
-    // **** value not found ****
-    return false;
-  }
-
-  /**
-   * Test scaffolding.
-   */
-  public static void main(String[] args) {
-
-    // **** open scanner ****
-    Scanner sc = new Scanner(System.in);
-
-    // **** read the contents for array a ****
-    String s = sc.nextLine();
-    String[] ss = s.split(" ");
-    int[] a = new int[ss.length];
-    for (int i = 0; i < a.length; i++) {
-      a[i] = Integer.parseInt(ss[i]);
-    }
-
-    // ???? ????
-    System.out.println("main <<< a: " + Arrays.toString(a));
-
-    // **** read the contents for array b ****
-    s = sc.nextLine();
-    ss = s.split(" ");
-    int[] b = new int[ss.length];
-    for (int i = 0; i < b.length; i++) {
-      b[i] = Integer.parseInt(ss[i]);
-    }
-
-    // ???? ????
-    System.out.println("main <<< b: " + Arrays.toString(b));
-
-    // **** read the value ****
-    int v = sc.nextInt();
-
-    // ???? ????
-    System.out.println("main <<< v: " + v);
-
-    // **** ****
-    System.out.println("main <<< " + sumOfTwo(a, b, v));
-    System.out.println("main <<< " + sumOfTwo1(a, b, v));
-
-    // **** close scanner ****
-    sc.close();
-  }
 }
